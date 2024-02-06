093579e9020dba749a8e2e09da7743d63887f3c3
Bruno Daniel <brunoflemos441@icloud.com>
2024-02-06 10:26:15 -0400

    first

---
+++ teste.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+       int b = 10;
+
+       int *ponteiro;
+
+       ponteiro = &b;
+
+       printf("%d", ponteiro);
+}
+
+
